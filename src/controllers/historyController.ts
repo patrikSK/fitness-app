376be14ed0f2e78fd42ef47bd5d22138c2980e57
patrik <husnajpatrik@gmail.com>
2022-09-29 00:23:54 +0200

    create getDates endpoint

--- src/controllers/historyController.ts
+++ src/controllers/historyController.ts
@@ -1,71 +1,119 @@
 import { Request, Response } from "express";
+import { Sequelize } from "sequelize/types";
 import { models } from "../db";
 import { getIdFromToken } from "../utils/auth";
 
 const { History } = models;
 
 const getCompletedExercises = async (_req: Request, res: Response) => {
-    const id = getIdFromToken(_req.headers.authorization);
+  const id = getIdFromToken(_req.headers.authorization);
 
-    try {
-        const records = await History.findAll({
-            where: { userId: id },
-        });
+  try {
+    const records = await History.findAll({
+      where: { userId: id },
+    });
 
-        return res.status(200).json({
-            success: true,
-            data: {
-                records: records,
-            },
-            message: "List of all completed exercises",
-        });
-    } catch (err) {
-        return res.status(400).json({ success: false, error: err });
-    }
+    return res.status(200).json({
+      success: true,
+      data: {
+        records: records,
+      },
+      message: "List of all completed exercises",
+    });
+  } catch (err) {
+    return res.status(400).json({ success: false, error: err });
+  }
+};
+
+const getDates = async (_req: Request, res: Response) => {
+  const id = getIdFromToken(_req.headers.authorization);
+
+  try {
+    const dates = await History.findAll({
+      attributes: ["date"],
+      group: "date",
+      where: { userId: id },
+    });
+
+    return res.status(200).json({
+      success: true,
+      data: {
+        dates: dates,
+      },
+      message: "List of all dates, when all exercises were performed",
+    });
+  } catch (err) {
+    return res.status(400).json({ success: false, error: err });
+  }
+};
+
+const getExercisesFromOneDay = async (_req: Request, res: Response) => {
+  const id = getIdFromToken(_req.headers.authorization);
+  const date = _req.params.date;
+
+  try {
+    const exercises = await History.findAll({
+      attributes: ["exerciseId", "exerciseName"],
+      where: { userId: id, date: date },
+      group: ["exerciseId"],
+    });
+
+    return res.status(200).json({
+      success: true,
+      data: {
+        exercises: exercises,
+      },
+      message: "List of all exercises, performed id one specific day",
+    });
+  } catch (err) {
+    return res.status(400).json({ success: false, error: err });
+  }
 };
 
 const addCompletedExercise = async (_req: Request, res: Response) => {
-    const id = getIdFromToken(_req.headers.authorization);
+  const id = getIdFromToken(_req.headers.authorization);
 
-    try {
-        const exercise = await History.create({
-            weight: _req.body.weight,
-            reps: _req.body.reps,
-            exerciseName: _req.body.exerciseName,
-            exerciseId: _req.body.exerciseId,
-            userId: id,
-        });
-        return res.status(200).json({
-            success: true,
-            exercise: exercise,
-            message: "exercise was succesfully saved to history",
-        });
-    } catch (err) {
-        return res.status(400).json({ success: false, error: err });
-    }
+  try {
+    const exercise = await History.create({
+      weight: _req.body.weight,
+      reps: _req.body.reps,
+      exerciseName: _req.body.exerciseName,
+      exerciseId: _req.body.exerciseId,
+      userId: id,
+    });
+    return res.status(201).json({
+      success: true,
+      exercise: exercise,
+      message: "exercise was succesfully saved to history",
+    });
+  } catch (err) {
+    return res.status(400).json({ success: false, error: err });
+  }
 };
 
 const removeExerciseFromHistory = async (_req: Request, res: Response) => {
-    const id = _req.params.id;
+  const id = _req.params.id;
 
-    try {
-        await History.destroy({
-            where: {
-                id: id,
-            },
-        });
+  try {
+    await History.destroy({
+      where: {
+        id: id,
+      },
+    });
 
-        return res.status(200).json({
-            success: true,
-            message: "Exercise was successfully removed from history",
-        });
-    } catch (err) {
-        return res.status(400).json({ success: false, error: err });
-    }
+    return res.status(200).json({
+      success: true,
+      message: "Exercise was successfully removed from history",
+    });
+  } catch (err) {
+    return res.status(400).json({ success: false, error: err });
+  }
 };
 
 export {
-    getCompletedExercises,
-    addCompletedExercise,
-    removeExerciseFromHistory,
+  getCompletedExercises,
+  addCompletedExercise,
+  removeExerciseFromHistory,
+  getDates,
+  getExercisesFromOneDay,
 };
--- src/routes/history.ts
+++ src/routes/history.ts
@@ -2,14 +2,18 @@ import { Router } from "express";
 import { checkAuthenticated } from "../controllers/authController";
 import {
   getCompletedExercises,
+  getDates,
   addCompletedExercise,
   removeExerciseFromHistory,
+  getExercisesFromOneDay,
 } from "../controllers/historyController";
 
 const router: Router = Router();
 
 export default () =>
   router
+    .get("/dates", checkAuthenticated, getDates)
+    .get("/:date", checkAuthenticated, getExercisesFromOneDay)
     .get("/", checkAuthenticated, getCompletedExercises)
     .post("/", checkAuthenticated, addCompletedExercise)
     .delete("/:id", checkAuthenticated, removeExerciseFromHistory);
