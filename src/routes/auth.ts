43a09c58fa80d1011ff48cd4031e72e671541ded
patrik <husnajpatrik@gmail.com>
2022-10-22 11:01:33 +0200

    remove unnecesary cors options

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,15 +1,7 @@
 import { Router } from "express";
 import handleRegister from "../controllers/registerController";
 import handleLogin from "../controllers/loginController";
-import cors from "cors";
-
-const corsOptions = {
-  origin: "*",
-  optionsSuccessStatus: 200,
-  credentials: true,
-};
 
 const router: Router = Router();
 
-export default () =>
-  router.post("/login", cors(corsOptions), handleLogin).post("/register", handleRegister);
+export default () => router.post("/login", handleLogin).post("/register", handleRegister);
